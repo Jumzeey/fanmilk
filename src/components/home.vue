2340bced96018facc48eaa660c1267b93cc32c08
jumzeey <umatonjayrex01@gmail.com>
2023-08-11 09:48:11 +0100

    first ciommit

---
+++ src/components/home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bg-blue-600">
+    this is the home
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
